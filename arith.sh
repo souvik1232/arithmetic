67b39b215223cd2d06ff9774e13dd342414248de
souvik1232 <souvik199831@gmail.com>
2020-11-19 12:06:16 +0530

    Added:Dictionary

--- arith.sh
+++ arith.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+declare -A arith
 read -p "Enter 3 numbers:" a b c
 calc1=$(( $a + ($b * $c) ))
 echo $calc1
@@ -8,3 +9,8 @@ calc3=$(( $c + ($a / $b) ))
 echo $calc3
 calc4=$(( ($a % $b) + $c ))
 echo $calc4
+arith[0]=$calc1
+arith[1]=$calc2
+arith[2]=$calc3
+arith[3]=$calc4
+echo "Dictionary--->"${arith[@]}
